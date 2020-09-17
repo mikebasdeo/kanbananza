590094d63eebfae6064905c26fea9d8c16981173
Mike Basdeo <mikebasdeo@gmail.com>
2020-09-17 15:58:11 -0400

    list-reducer and card-reducer. Gnawrly!

--- src/reducers/card-reducer.js
+++ src/reducers/card-reducer.js
@@ -2,6 +2,13 @@ import { cards as defaultCards } from '../normalized-state'
 
 const cardReducer = (cards = defaultCards, action) => {
   console.log(`helllo ${cards}, ${action}`)
+  if (action.type === 'CARD_CREATE') {
+    const { card, cardId } = action.payload
+    return {
+      entities: { ...cards.entities, [cardId]: card },
+      ids: [...cards.ids, cardId],
+    }
+  }
   return cards
 }
 
