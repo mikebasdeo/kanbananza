06478fdf7f62c9d4197c78fa87b330a592d7425e
Mike Basdeo <mikebasdeo@gmail.com>
2020-09-18 13:39:35 -0400

    still working

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 // import Card from './Card';
 // import CreateCard from './CreateCard';
@@ -6,17 +6,18 @@ import CreateCardContainer from '../containers/CreateCardContainer'
 import CardContainer from '../containers/CardContainer'
 
 const List = ({ list = {}, removeList }) => {
+  console.log(JSON.stringify(list, null, 2))
   return (
     <article className="List">
       <h2>{list.title}</h2>
-      <CreateCardContainer listId={list.id}/>
+      <CreateCardContainer listId={list.id} />
       <div>
-        {list.cards.map(cardId => (
+        {list.cards.map((cardId) => (
           <CardContainer key={cardId} cardId={cardId} listId={list.id} />
         ))}
       </div>
     </article>
-  );
-};
+  )
+}
 
-export default List;
+export default List
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ListContainer from '../containers/ListContainer'
 
+// critique #1 of redux. That @param lists comes from redux is not obvious (container is the hint).
 const Lists = ({ lists = [] }) => {
   return (
     <section className="Lists">
