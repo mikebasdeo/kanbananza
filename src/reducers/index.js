4d5d73c088da194f46644eb45fe2fd001a05b421
Mike Basdeo <mikebasdeo@gmail.com>
2020-09-18 18:09:29 -0400

    added users to redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux'
 
 import lists from './list-reducer'
 import cards from './card-reducer'
+import users from './user-reducer'
 
-export default combineReducers({ lists, cards })
+export default combineReducers({ lists, cards, users })
