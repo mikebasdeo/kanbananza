7dca7ef31ac2a95bbbb5f074ab334f5f2ad3ba7a
Mike Basdeo <mikebasdeo@gmail.com>
2020-09-19 12:54:35 -0400

    still working

--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -1,31 +1,29 @@
-import { connect } from 'react-redux';
-import Card from '../components/Card';
-import { removeCard } from '../actions/card-actions';
-import { createSelector } from 'reselect';
-import memoize from 'lodash/memoize';
+import { connect } from 'react-redux'
+import Card from '../components/Card'
+import { removeCard } from '../actions/card-actions'
+import { createSelector } from 'reselect'
+import memoize from 'lodash/memoize'
 
-const getListId = memoize(cardId =>
+const getListId = memoize((cardId) =>
   createSelector(
-    state => state.lists.entities,
-    lists => {
-      console.log('findListIdForCard', lists, cardId);
+    (state) => state.lists.entities,
+
+    (lists) => {
+      console.log('findListIdForCard', lists, cardId)
       for (const [listId, list] of Object.entries(lists)) {
         if (list.cards.includes(cardId)) {
-          return listId;
+          return listId
         }
       }
-    },
-  ),
-);
+    }
+  )
+)
 
 const mapStateToProps = (state, ownProps) => {
   return {
     card: state.cards.entities[ownProps.cardId],
     listId: getListId(ownProps.cardId)(state),
-  };
-};
+  }
+}
 
-export default connect(
-  mapStateToProps,
-  { removeCard },
-)(Card);
+export default connect(mapStateToProps, { removeCard })(Card)
